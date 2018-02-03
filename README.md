d73c5f38dcdd97dbcc362ea74d81b89c3675cc4c
MartyJiang <taojiang48@gmail.com>
2018-02-03 23:31:48 +0800

    add iterate support.

--- README.md
+++ README.md
@@ -4,10 +4,17 @@ Emit the last value emitted from source on completion.
 
 ```javascript
 const { pipe, interval, take, observe } = require('callbag-basics');
+
 pipe(
   interval(100),
   take(5),
   last(),
   observe(v => console.log(v)) // 4
+);
+
+pipe(
+  fromIter([1, 2, 3, 4]),
+  last(),
+  iterate(v => console.log(v)) // 4
 );
 ```
--- index.js
+++ index.js
@@ -2,19 +2,21 @@ const last = () => source => (start, slink) => {
   if (start !== 0) return;
   let talkback;
   let lastVal;
+  let lasted = false;
   source(0, (t, d) => {
     if (t === 0) {
       talkback = d;
+      slink(t, d);
     } else if (t === 1) {
       lastVal = d;
+      talkback(1);
     } else if (t === 2) {
+      if (lasted) return;
+      lasted = true;
       slink(1, lastVal);
-      talkback(2);
       slink(2);
     } else {
       slink(t, d);
     }
   });
 };
-
-module.exports = last;
